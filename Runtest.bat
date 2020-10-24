4e9ed3fa9c476d026f3b79395c7974e0f17e48c9
autoak <73205679+autoak@users.noreply.github.com>
2020-10-24 21:55:27 +1300

    Update Runtest.bat
    Replaced outputdir with message. This needs to be changed to your local folder path after cloning the repo

--- Runtest.bat
+++ Runtest.bat
@@ -1,6 +1,6 @@
 set BROWSER=chrome
 set PROJECT_URL=https://www.westpac.co.nz
-set outputdir=C:\Users\Y982410\PycharmProjects\test\testing1
+set outputdir= "give your local folder path of your drive where you cloned this repo"
 set Testlink_Tag=T
 set Jenkins_Build_ID=1
 set CODEBUILD_BUILD_ID=test
