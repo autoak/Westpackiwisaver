18524a85c1e5b50e0582c224a6a5f02c634c095c
autoak <73205679+autoak@users.noreply.github.com>
2020-10-24 21:30:14 +1300

    Update Runtest.bat

--- Runtest.bat
+++ Runtest.bat
@@ -4,7 +4,5 @@ set outputdir=C:\Users\Y982410\PycharmProjects\test\testing1
 set Testlink_Tag=T
 set Jenkins_Build_ID=1
 set CODEBUILD_BUILD_ID=test
-
-DEL "C:\Test Automation\Beryllium2\beryllium_regression_test\*.png"
 robot      --include   codebuild_ready   *\tests.robot
 cmd.exes
