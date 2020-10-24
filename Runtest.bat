242a318f9cc785c0bce95d1299ad08fd41d4a1e4
autoak <73205679+autoak@users.noreply.github.com>
2020-10-24 22:19:02 +1300

    Update Runtest.bat
    Removed unwanted code lines

--- Runtest.bat
+++ Runtest.bat
@@ -1,8 +1,5 @@
 set BROWSER=chrome
 set PROJECT_URL=https://www.westpac.co.nz
 set outputdir= "give your local folder path of your drive where you cloned this repo"
-set Testlink_Tag=T
-set Jenkins_Build_ID=1
-set CODEBUILD_BUILD_ID=test
 robot      --include   codebuild_ready   *\tests.robot
 cmd.exes
